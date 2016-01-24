8b729a92814807267af0454ca1128b3cabdde10a
Ashay Sheth <ashay.sheth@gmail.com>
2016-01-24 06:53:54 -0500

    added general commit history to site (not formated)

--- js/script.js
+++ js/script.js
@@ -154,65 +154,143 @@ $(document).ready(function(){
 
 
 
+// var margin = {top: 0, right: 40, bottom: 60, left: 40},
+//     width = 750,
+//     height = 500 - margin.top - margin.bottom;
+
+// var parseDate = d3.time.format("%Y%m%d").parse;
+
+// var x = d3.time.scale()
+//     .range([0, width]);
+
+// var y = d3.scale.linear()
+//     .range([height, 0]);
+
+// var color = d3.scale.category10();
+
+// var xAxis = d3.svg.axis()
+//     .scale(x)
+//     .orient("bottom");
+
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left");
+
+// var line = d3.svg.line()
+//     .interpolate("basis")
+//     .x(function(d) { return x(d.date); })
+//     .y(function(d) { return y(d.temperature); });
+
+// var svg = d3.select("#gitActivity").append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//   .append("g")
+//     .attr("transform", "translate(" + margin.left+ ", 0)");
+
+// d3.tsv("data.tsv", function(error, data) {
+//   if (error) {
+//     throw error; 
+//   }
+
+//   color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+//   data.forEach(function(d) {
+//     d.date = parseDate(d.date);
+//   });
+
+//   var cities = color.domain().map(function(name) {
+//     return {
+//       name: name,
+//       values: data.map(function(d) {
+//         return {date: d.date, temperature: +d[name]};
+//       })
+//     };
+//   });
+
+//   x.domain(d3.extent(data, function(d) { return d.date; }));
+
+//   y.domain([
+//     d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
+//     d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+//   ]);
+
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis);
+
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis);
+//     // .append("text")
+//     //   .attr("transform", "rotate(-90)")
+//     //   .attr("y", 6)
+//     //   .attr("dy", ".71em")
+//     //   .style("text-anchor", "end")
+//     //   .text("Temperature (ºF)");
+
+//   var city = svg.selectAll(".city")
+//       .data(cities)
+//     .enter().append("g")
+//       .attr("class", "city");
+
+//   city.append("path")
+//       .attr("class", "line")
+//       .attr("d", function(d) { return line(d.values); })
+//       .style("stroke", function(d) { return color(d.name); });
+
+//   city.append("text")
+//       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+//       .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+//       .attr("x", 3)
+//       .attr("dy", ".35em")
+//       .text(function(d) { return d.name; });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var margin = {top: 0, right: 40, bottom: 60, left: 40},
     width = 750,
-    height = 500 - margin.top - margin.bottom;
+    height = 500;
 
-var parseDate = d3.time.format("%Y%m%d").parse;
+var x = d3.scale.linear().range([0, width]);
 
-var x = d3.time.scale()
-    .range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
 
-var color = d3.scale.category10();
+var yAxis = d3.svg.axis().scale(y).orient("left");
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+var line = d3.svg.line().x(function(d) { return x(d.timeS); }).y(function(d) { return y(d.commits); });
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+var svg = d3.select("#gitActivity").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.temperature); });
+d3.tsv("data/generalGit.tsv", type, function(error, data) {
+  if (error) throw error;
 
-var svg = d3.select("#gitActivity").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + margin.left+ ", 0)");
-
-d3.tsv("data/data.tsv", function(error, data) {
-  if (error) {
-    throw error; 
-  }
-
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-  });
-
-  var cities = color.domain().map(function(name) {
-    return {
-      name: name,
-      values: data.map(function(d) {
-        return {date: d.date, temperature: +d[name]};
-      })
-    };
-  });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-
-  y.domain([
-    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-  ]);
+  x.domain(d3.extent(data, function(d) { return d.timeS; }));
+  y.domain(d3.extent(data, function(d) { return d.commits; }));
 
   svg.append("g")
       .attr("class", "x axis")
@@ -221,31 +299,26 @@ d3.tsv("data/data.tsv", function(error, data) {
 
   svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis);
-    // .append("text")
-    //   .attr("transform", "rotate(-90)")
-    //   .attr("y", 6)
-    //   .attr("dy", ".71em")
-    //   .style("text-anchor", "end")
-    //   .text("Temperature (ºF)");
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Commits");
 
-  var city = svg.selectAll(".city")
-      .data(cities)
-    .enter().append("g")
-      .attr("class", "city");
-
-  city.append("path")
+  svg.append("path")
+      .datum(data)
       .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.name); });
-
-  city.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+      .attr("d", line);
 });
+
+function type(d) {
+  // d.date = formatDate.parse(d.date);
+  d.timeS = parseInt(d.timeS);
+  d.commits = d.commits;
+  return d;
+}
 
 
 });
